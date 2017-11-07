c1f421851f9fa866a5d6db470b2450a8d6560227
Lawlzilla <Lawlzilla@gmail.com>
2017-11-07 12:22:40 +0100

    Heel project
    Gameplay p5 van Furkan tura

---
+++ Topdown-adventure/New Unity Project/Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float M_speed = 4f;
+
+    Vector3 Forward, Right;
+
+    Rigidbody rb;
+
+	void Start () {
+        Forward = Camera.main.transform.forward;
+        Forward.y = 0;
+        Forward = Vector3.Normalize(Forward);
+        Right = Quaternion.Euler(new Vector3(0, 90, 0)) * Forward;
+	}
+
+   
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Move();
+        }
+
+    }
+
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorKey"), 0, Input.GetAxis("VerKey"));
+        Vector3 rightMov = Right * M_speed * Time.deltaTime * Input.GetAxis("HorKey");
+        Vector3 upMov = Forward * M_speed * Time.deltaTime * Input.GetAxis("VerKey");
+
+        Vector3 heading = Vector3.Normalize(rightMov + upMov);
+        transform.forward = heading;
+        transform.position += rightMov;
+        transform.position += upMov;
+
+    }
+
+  
+
+}
---
+++ Topdown-adventure/New Unity Project/Assets/scripts/colliderbox.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderbox : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Door;
+
+    [SerializeField]
+    bool Isactive = false;
+
+    [SerializeField]
+    bool IsDoor = false;
+
+    
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float Angle;
+
+    [SerializeField]
+    float AngleBack;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+       
+
+        if (Isactive == true)
+        {
+            //Door.transform.rotation = Quaternion.Lerp(From.rotation, To.rotation, Time.time * speed);
+            Door.transform.rotation = Quaternion.AngleAxis(Angle, Vector3.up);
+        }
+        else
+        {
+            Door.transform.rotation = Quaternion.AngleAxis(AngleBack, Vector3.up);
+        }
+
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.collider.tag == "krat")
+        {
+            Isactive = true;
+        }
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "krat")
+        {
+            Isactive = false;
+        }
+    }
+
+
+}
